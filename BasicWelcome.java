d94e6cc61039522b84eb38be4486971c6f2f9b19
mrunal08 <mrunalg96@gmail.com>
2021-08-17 10:13:52 +0530

    Welcome to Basics Index

---
+++ BasicWelcome.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class BasicWelcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Basic Core Programs");
+        System.out.println("UC1 : Flip Coin");
+        System.out.println("UC2 : Leap Year");
+        System.out.println("UC3 : Power of 2");
+        System.out.println("UC4 : Harmonic Number");
+        System.out.println("UC5 : Factors");
+        System.out.println("UC6 : Quotient and Remainder");
+        System.out.println("UC7 : Swap Two Numbers");
+        System.out.println("UC8 : Even Odd Numbers");
+        System.out.println("UC9 : Vowels and Consonents");
+        System.out.println("UC10 : Largest");
+    }
+}
